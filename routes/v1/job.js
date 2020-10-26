c3c6d0edefe3b54611e82f4bf0ff8d4d46a095dd
yisacc <yisacc.abmyfather@gmail.com>
2020-10-26 17:20:06 +0300

    some documentation issue fix

--- routes/v1/job.js
+++ routes/v1/job.js
@@ -3,26 +3,25 @@ var router = express.Router();
 var {addNewJob,getJobById,fetchJobs,editJob,deleteJob}=require('../../controllers/jobController');
 const { hasPermissions } = require('../../middlewares/auth');
 
-/* GET users listing. */
 /**
- * @typedef USER
- * @property {string} jobTitle.required 
- * @property {string} email.required 
- * @property {string} password.required 
- */
-/**
- * Returns ALL Users
+ * Create a new user 
  * 
- * @route GET /users
- * @group User - Deals with all CRUD operations with user model
- * @param {string} sort.query - sort parament
- * @param {string} page.query - set the page number
- * @param {string} filter.query - set filter query 
+ * @route POST /users/
+ * @group Job 
+ * @param {Job.model} Job.body.required - the new user
  * @security JWT
- * @returns {object} 200 - Array of users
+ * @returns {object} 200 - User object
  * @returns {Error}  default - Unexpected error
  */
 router.route('/create').post(hasPermissions(['create job']),addNewJob);
+
+/**
+ * This function comment is parsed by doctrine
+ * @route GET /api
+ * @group foo - Operations about Job
+ * @returns {object} 200 - An array of Job info
+ * @returns {Error}  default - Unexpected error
+ */
 router.route('/getJobs').get(fetchJobs);
 router.route('/getJobById').get(getJobById);
 router.route('/editJob').put(hasPermissions(['edit job']),editJob);
